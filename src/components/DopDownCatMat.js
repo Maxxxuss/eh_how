06dff7cd0095b93ea2d585d319de93a4e7b1ddab
Maxxxuss <newsfoxxx@googlemail.com>
2021-08-07 12:32:39 +0200

    reworks MAt UI categories selection

--- src/components/DopDownCatMat.js
+++ src/components/DopDownCatMat.js
@@ -1,102 +1,73 @@
-import React from 'react';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-import TextField from '@material-ui/core/TextField';
+// import React from 'react';
+// import InputLabel from '@material-ui/core/InputLabel';
+// import FormControl from '@material-ui/core/FormControl';
+// import TextField from '@material-ui/core/TextField';
 
-import Select from '@material-ui/core/Select';
-import { MenuItem } from 'semantic-ui-react';
-import { Autocomplete } from '@material-ui/lab';
-import SelectInput from '@material-ui/core/Select/SelectInput';
-
-
-
-class DropDownCat extends React.Component {
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            categorie: []
-
-        }
-    }
-
-    handleChange = (event) => {
-        const name = event.target.name;
-        setState({
-            ...state,
-            [name]: event.target.value,
-        });
-    };
+// import Select from '@material-ui/core/Select';
+// import { MenuItem } from 'semantic-ui-react';
+// import { Autocomplete } from '@material-ui/lab';
+// import SelectInput from '@material-ui/core/Select/SelectInput';
 
 
 
+// class DropDownCat extends React.Component {
 
-    catCheck = () => {
-        console.log("Cat State", this.state.categorie
-        );
-    }
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             categorie: []
 
+//         }
+//     }
 
-    onCateChange = (e) => {
-        const categorie = e.target.value
-        this.setState(() => ({ categorie }))
-        console.log(this.state.categorie);
-
-
-    }
-
-
-    render() {
-        const { categories, activeNote } = this.props
-        const { categorie } = this.state
-
-        return (
-            <div>
-
-
-                <Autocomplete
-                    id="combo-box-demo"
-
-
-                    options={categories}
-                    getOptionLabel={(option) => (option ? option.catName : [])}
-                    inputValue = {categorie.toString()}
-                    // onInputChange={this.onCateChange}
-                    onInputChange={( event, categorie) =>{
-                        this.setState({categorie})
-                    }}
-                    onChange={this.onCateChange}
-
-
-                    style={{ width: 300 }}
-                    renderInput={(params) =>
-                        <TextField {...params}
-                            label="Combo box"
-                            variant="outlined"
-
-
-                        />}
-                />
-
-                <div>
-                    <button
-                        onClick={this.catCheck}
-                    >
-
-                        cat Checl
-                    </button>
-
-                </div>
-
-            </div>
+//     handleChange = (event) => {
+//         const name = event.target.name;
+//         setState({
+//             ...state,
+//             [name]: event.target.value,
+//         });
+//     };
 
 
 
+//     onCateChange = (e) => {
+//         const categorie = e.target.value
+//         this.setState(() => ({ categorie }))
+//         console.log(this.state.categorie);
+//     }
 
-        )
-    }
-}
+
+//     render() {
+//         const { categories, activeNote } = this.props
+//         const { categorie } = this.state
+
+//         return (
+//             <div>
+
+//                 <Autocomplete
+//                     id="combo-box-demo"
+//                     options={categories}
+//                     getOptionLabel={(option) => (option ? option.catName : [])}
+//                     inputValue = {categorie.toString()}
+//                     // onInputChange={this.onCateChange}
+//                     onInputChange={( event, categorie) =>{
+//                         this.setState({categorie})
+//                     }}
+//                     onChange={this.onCateChange}
+
+//                     // style={{ width: 300 }}
+//                     renderInput={(params) =>
+//                         <TextField {...params}
+//                             label="Combo box"
+//                             variant="outlined"
+//                         />}
+//                 />
+//             </div>
+
+//         )
+//     }
+// }
 
 
 
-export default DropDownCat
+// export default DropDownCat
